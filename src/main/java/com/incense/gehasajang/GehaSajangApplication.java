786350ceb5f44ee52277f93326f0e56d15b67369
beemiel <qnswwrn@gmail.com>
2020-07-09 01:41:13 +0900

    Initial commit

---
+++ src/main/java/com/incense/gehasajang/GehaSajangApplication.java
@@ -0,0 +1,13 @@
+package com.incense.gehasajang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GehaSajangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GehaSajangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incense/gehasajang/GehaSajangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incense.gehasajang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GehaSajangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
