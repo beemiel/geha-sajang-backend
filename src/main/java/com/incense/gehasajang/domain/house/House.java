a5776d21d57e2e74b68544ef3ab6f9f6de5bfa27
beemiel <qnswwrn@gmail.com>
2020-08-11 01:30:41 +0900

    feat: house 관련 entity 작성
    - House, HouseExtraInfo, HouseOff, Address
    - Address는 Host에도 사용하는 ValueType으로 구현

---
+++ src/main/java/com/incense/gehasajang/domain/house/House.java
@@ -0,0 +1,52 @@
+package com.incense.gehasajang.domain.house;
+
+import com.incense.gehasajang.domain.Address;
+import com.incense.gehasajang.domain.Booking;
+import com.incense.gehasajang.domain.HostHouse;
+import com.incense.gehasajang.domain.Invitation;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+public class House {
+
+    @Id @GeneratedValue
+    @Column(name = "house_id")
+    private Long id;
+
+    private String uuid;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    private String main_image;
+
+    private String main_number;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "house")
+    private List<HostHouse> hostHouses;
+
+    @OneToMany(mappedBy = "house")
+    private List<HouseExtraInfo> houseExtraInfos;
+
+    @OneToMany(mappedBy = "house")
+    private List<HouseOff> houseOffs;
+
+    @OneToMany(mappedBy = "house")
+    private List<Invitation> invitations;
+
+    @OneToMany(mappedBy = "house")
+    private List<Booking> bookings;
+}
