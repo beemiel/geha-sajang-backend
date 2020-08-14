57b8481d8899ee1d46c955f09a1b4f02fa731e24
Max Kim <bluuminn@gmail.com>
2020-08-14 18:50:44 +0900

    feat: RoomController 생성

---
+++ src/main/java/com/incense/gehasajang/controller/RoomController.java
@@ -0,0 +1,9 @@
+package com.incense.gehasajang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomController {
+}
