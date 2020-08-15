65cb33b5707e4e35a900612f69726f200eda418d
beemiel <qnswwrn@gmail.com>
2020-08-15 21:01:09 +0900

    feat: ErrorMessage를 모아둔 상수 클래스 추가 및 적용
    ErrorMessages
      - 상수 클래스 추가
    HouseService
      - 기존의 에러 메세지를  상수로 변경

--- src/main/java/com/incense/gehasajang/service/HouseService.java
+++ src/main/java/com/incense/gehasajang/service/HouseService.java
@@ -3,6 +3,7 @@ package com.incense.gehasajang.service;
 import com.incense.gehasajang.domain.HouseRepository;
 import com.incense.gehasajang.domain.house.House;
 import com.incense.gehasajang.exception.NotFoundDataException;
+import com.incense.gehasajang.util.ErrorMessages;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ public class HouseService {
     private HouseRepository houseRepository;
 
     public House getHouse(Long houseId) {
-        return houseRepository.findById(houseId).orElseThrow(() -> new NotFoundDataException("데이터를 찾을 수 없습니다.", HttpStatus.NOT_FOUND));
+        return houseRepository.findById(houseId)
+                .orElseThrow(() -> new NotFoundDataException(ErrorMessages.NOT_FOUND, HttpStatus.NOT_FOUND));
     }
 }
