82de779b4607ae086021050f126586796569950b
Max Kim <bluuminn@gmail.com>
2020-08-17 02:26:16 +0900

    feat: RoomController get 메서드 도출

--- src/main/java/com/incense/gehasajang/controller/RoomController.java
+++ src/main/java/com/incense/gehasajang/controller/RoomController.java
@@ -1,9 +1,20 @@
 package com.incense.gehasajang.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
+
+    @GetMapping
+    public void list() {
+        // TODO: 방의 전체 리스트
+    }
+
+    @GetMapping("/{roomsId}")
+    public void detail() {
+        // TODO: roomId 에 해당하는 room 조회
+    }
 }
