96de23cb4dee0044a1e6aa62155cb72a654e3821
beemiel <qnswwrn@gmail.com>
2020-08-22 05:04:22 +0900

    feat: HouseExtraInfo insert Servic 및 repo  추가
    - 문자열로 받은HouseExtraInfo를 객체로 만든 뒤 저장
    - 빈 문자열일 경우 예외처리 추가

---
+++ src/main/java/com/incense/gehasajang/domain/house/HouseExtraInfoRepository.java
@@ -0,0 +1,6 @@
+package com.incense.gehasajang.domain.house;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseExtraInfoRepository extends JpaRepository<HouseExtraInfo, Long> {
+}
