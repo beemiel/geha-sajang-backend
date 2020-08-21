2d34b7da4a6ef7ff40137885bc66ee76195cbdad
beemiel <qnswwrn@gmail.com>
2020-08-22 04:58:03 +0900

    feat: 빈 이미지 필드 예외처리, 업로드 크기 조정

--- src/main/java/com/incense/gehasajang/service/S3Service.java
+++ src/main/java/com/incense/gehasajang/service/S3Service.java
@@ -25,6 +25,10 @@ public class S3Service {
     private String bucket;
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        if(multipartFile.isEmpty()){
+            return "";
+        }
+
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException(("MultipartFile -> File로 전환이 실패했습니다.")));
 
