c7c01ef9233fce3ea9236eb08c2834ad30b7e4e7
beemiel <qnswwrn@gmail.com>
2020-08-23 01:39:46 +0900

    feat: config에 cors 설정 추가

--- src/main/java/com/incense/gehasajang/util/SpringConfig.java
+++ src/main/java/com/incense/gehasajang/util/SpringConfig.java
@@ -3,6 +3,7 @@ package com.incense.gehasajang.util;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,16 @@ public class SpringConfig implements WebMvcConfigurer {
                 .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
                 .findFirst()
                 .ifPresent(converter -> ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(UTF_8));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        long MAX_AGE_SECS = 3600;
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
