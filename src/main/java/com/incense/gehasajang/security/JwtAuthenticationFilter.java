daeaf0bf6a3a315c6a982ca80da4cf01539e6f3b
beemiel <qnswwrn@gmail.com>
2020-08-28 00:16:48 +0900

    feat: Jwt Filter 추가, 커스텀 인증 객체 추가
    JwtAuthenticationFilter
      - 권한이 필요한 모든 요청에 실행되는 필터. Jwt 토큰 인증에 사용
      - Jwt 토큰이 있는지 확인하고 파싱하여 커스텀 인증 객체를 컨텍스트에
      추가함
    
    UserAuthentication
      - 커스텀 유저 인증 객체
      - Jwt 토큰에서 가져온 Claims객체를 갖고 있다(account, role)
      - 여기서 로그인 한 유저의 역할이 정해짐

---
+++ src/main/java/com/incense/gehasajang/security/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.incense.gehasajang.security;
+
+import com.incense.gehasajang.util.JwtProperties;
+import com.incense.gehasajang.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+    }
+
+    /**
+     * 요청을 처리하기 전 이 필터가 실행됨
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        Authentication authentication = getAuthentication(request);
+
+        if(authentication != null) {
+            //Controller에서 Authentication 객체를 쓰기 위해 보안 컨텍스트에 인증 객체를 넣어줌
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+
+        chain.doFilter(request,response);
+    }
+
+    /**
+     * jwt 토큰을 확인하고 인증 객체를 만들어 반환한다.
+     */
+    private Authentication getAuthentication(HttpServletRequest request) {
+        String authorization = request.getHeader(JwtProperties.HEADER_STRING);
+        if(authorization == null) {
+            return null;
+        }
+
+        String token = authorization.substring(JwtProperties.TOKEN_PREFIX.length());
+        Claims claims = jwtUtil.parseToken(token);
+
+        return new UserAuthentication(claims);
+    }
+}
