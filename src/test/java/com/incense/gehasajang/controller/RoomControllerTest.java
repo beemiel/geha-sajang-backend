c76eeba5284d51f0e6a809da40d8758f2c072ce4
beemiel <qnswwrn@gmail.com>
2020-08-28 16:06:43 +0900

    refactor: 테스트 코드에서 불필요한 부분 제거

--- src/test/java/com/incense/gehasajang/controller/RoomControllerTest.java
+++ src/test/java/com/incense/gehasajang/controller/RoomControllerTest.java
@@ -1,9 +1,10 @@
 package com.incense.gehasajang.controller;
 
+import com.incense.gehasajang.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -18,13 +19,14 @@ class RoomControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private RoomService roomService;
+
     @Test
     public void list() throws Exception {
-        mockMvc.perform(get("/rooms")
-                .accept(MediaType.APPLICATION_JSON_UTF8))
+        mockMvc.perform(get("/rooms"))
                 .andExpect(status().isOk())
                 .andExpect(content().encoding("UTF-8"))
         .andExpect(content().string(containsString("\"room\":\"test\"")));
-
     }
 }
