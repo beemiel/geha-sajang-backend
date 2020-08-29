9a42faa12b8c1e3a3f8b3d76188d6949f9840eaa
Max Kim <bluuminn@gmail.com>
2020-08-30 06:05:10 +0900

    feat: Host 검증 메서드 추가
    - 로그인 한 Host의 방 정보를 요청한게 맞는지 검증

--- src/main/java/com/incense/gehasajang/service/RoomService.java
+++ src/main/java/com/incense/gehasajang/service/RoomService.java
@@ -1,7 +1,9 @@
 package com.incense.gehasajang.service;
 
+import com.incense.gehasajang.domain.host.HostRepository;
 import com.incense.gehasajang.domain.room.Room;
 import com.incense.gehasajang.domain.room.RoomRepository;
+import com.incense.gehasajang.exception.AccessDeniedException;
 import com.incense.gehasajang.exception.NotFoundDataException;
 import com.incense.gehasajang.model.param.room.RoomDetailParam;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,20 @@ import java.util.List;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final HostRepository hostRepository;
 
     public List<Room> getRooms(Long houseId) {
         return roomRepository.findAllByHouse_Id(houseId);
     }
 
     public Room getRoom(RoomDetailParam detailParam) {
-        return roomRepository.findById(detailParam.getRoomId()).orElseThrow(NotFoundDataException::new);
+        authorityCheck(detailParam);
+        return roomRepository.findById(detailParam.getRoomId())
+                .orElseThrow(NotFoundDataException::new);
+    }
+
+    private void authorityCheck(RoomDetailParam detailParam) {
+        hostRepository.findHouseByAccountAndHouseId(detailParam.getAccount(), detailParam.getHouseId())
+                .orElseThrow(AccessDeniedException::new);
     }
 }
