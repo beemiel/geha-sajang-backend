09c8531f85b2612963286a2ddbbb1a6d9020f7c5
beemiel <qnswwrn@gmail.com>
2020-08-31 23:08:43 +0900

    feat: join fetch Test 추가
    - 기존의 HostRepositoryTest와 분리함
      이유는 롤백의 유무때문애. join fetch를 사용한 메소드 테스트 시에
      롤백을
      하게 되면 조인한 테이블의 데이터가 null로 가져와짐. 그래서 일단
      분리시킴

---
+++ src/test/java/com/incense/gehasajang/domain/host/HostRepositoryFetchTest.java
@@ -0,0 +1,73 @@
+package com.incense.gehasajang.domain.host;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
+@DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+class HostRepositoryFetchTest {
+
+    @Autowired
+    private HostRepository hostRepository;
+
+    @Autowired
+    private HostAuthKeyRepository hostAuthKeyRepository;
+
+    @BeforeEach
+    public void setUp() {
+        List<Host> hosts = Arrays.asList(
+                MainHost.builder().account("maxmax@gmail.com").nickname("max").password("maxmax123").isAgreeToMarketing(true).build(),
+                MainHost.builder().account("joyjoy@gmail.com").nickname("joy").password("joyjoy123123").isAgreeToMarketing(true).build()
+        );
+        hosts.forEach(host -> hostRepository.save(host));
+
+        hosts.forEach(host -> {
+            hostAuthKeyRepository.save(HostAuthKey.builder()
+                    .authKey(host.getAccount())
+                    .host(host)
+                    .expirationDate(LocalDateTime.now().plusDays(1))
+                    .build());
+        });
+    }
+
+    @AfterEach
+    public void deleteAll() {
+        hostAuthKeyRepository.deleteAll();
+        hostRepository.deleteAll();
+    }
+
+    @ValueSource(strings = {"maxmax@gmail.com", "joyjoy@gmail.com"})
+    @DisplayName("인증할 호스트 찾기 테스트")
+    @ParameterizedTest(name = "{index}/{arguments} TEST")
+    void findHostTest(String email) throws Exception {
+        //when
+        MainHost host = hostRepository.findMainHostByAccount(email).orElseGet(
+                () -> MainHost.builder().account("null").build()
+        );
+
+        //then
+        assertAll(
+                () -> assertThat(host.getAccount()).isEqualTo(email),
+                () -> assertThat(host.getDeletedAt()).isNull(),
+                () -> assertThat(host.getAuthKey().getAuthKey()).isEqualTo(email),
+                () -> assertThat(host.isAgreeToMarketing()).isTrue()
+        );
+
+    }
+
+}
