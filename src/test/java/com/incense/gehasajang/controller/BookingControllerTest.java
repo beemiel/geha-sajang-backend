7e610ce092b6a8c61857e85ea7ae1556f73d36b6
beemiel <qnswwrn@gmail.com>
2020-09-08 03:53:29 +0900

    feat: BookingControllerTest 추가 및 Api 문서 추가
    BookingControllerTest
      - 예외에 대한 테스트 추가
    
    Api docs
      - 예외에 대한 내용 추가
      - ErrorCode 추가

--- src/test/java/com/incense/gehasajang/controller/BookingControllerTest.java
+++ src/test/java/com/incense/gehasajang/controller/BookingControllerTest.java
@@ -2,12 +2,16 @@ package com.incense.gehasajang.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.incense.gehasajang.domain.host.HostRole;
+import com.incense.gehasajang.error.ErrorCode;
+import com.incense.gehasajang.exception.NotFoundDataException;
+import com.incense.gehasajang.exception.ZeroCountException;
 import com.incense.gehasajang.model.dto.booking.request.BookingExtraInfoRequestDto;
 import com.incense.gehasajang.model.dto.booking.request.BookingRequestDto;
 import com.incense.gehasajang.model.dto.booking.request.BookingRoomRequestDto;
 import com.incense.gehasajang.model.dto.guest.request.GuestRequestDto;
 import com.incense.gehasajang.security.UserAuthentication;
-import com.incense.gehasajang.service.*;
+import com.incense.gehasajang.service.AuthorizationService;
+import com.incense.gehasajang.service.BookingService;
 import com.incense.gehasajang.util.CommonString;
 import com.incense.gehasajang.util.JwtUtil;
 import io.jsonwebtoken.Claims;
@@ -25,10 +29,13 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -36,6 +43,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookingController.class)
@@ -96,39 +104,142 @@ class BookingControllerTest {
         ResultActions resultActions = create(request);
 
         //then
-        resultActions.andExpect(status().isCreated()).andDo(document("{class-name}/{method-name}",
-                preprocessRequest(modifyUris()
-                        .scheme(CommonString.SCHEMA)
-                        .host(CommonString.HOST), prettyPrint()),
-                preprocessResponse(prettyPrint()),
-                pathParameters(
-                        parameterWithName("houseId").description("요청하고자 하는 house id, 호스트가 속한 하우스가 아닐 경우 403 반환")
-                ),
-                requestFields(
-                        fieldWithPath("bookingId").description("예약 id"),
-                        fieldWithPath("checkIn").description("체크인 날짜. 필수"),
-                        fieldWithPath("checkOut").description("체크아웃 날짜. 필수"),
-                        fieldWithPath("requirement").description("요구사항. 500자 이내"),
-                        fieldWithPath("guest").description("게스트 Request 객체"),
-                        fieldWithPath("guest.guestId").description("게스트 id"),
-                        fieldWithPath("guest.name").description("게스트 이름. 필수"),
-                        fieldWithPath("guest.phoneNumber").description("게스트 전화번호. 필수"),
-                        fieldWithPath("guest.email").description("게스트 이메일"),
-                        fieldWithPath("guest.memo").description("게스트 메모. 500자 이내"),
-                        fieldWithPath("bookingRooms").description("예약한 방 배열"),
-                        fieldWithPath("bookingRooms[].roomId").description("선택한 방의 id. 필수"),
-                        fieldWithPath("bookingRooms[].femaleCount").description("여성 인원수"),
-                        fieldWithPath("bookingRooms[].maleCount").description("남성 인원수"),
-                        fieldWithPath("bookingExtraInfos").description("작성된 하우스 추가 서비스 배열"),
-                        fieldWithPath("bookingExtraInfos[].houseExtraInfoId").description("선택한 추가 서비스 id. 필수"),
-                        fieldWithPath("bookingExtraInfos[].bookingExtraInfoId").description("예약 추가 서비스 id"),
-                        fieldWithPath("bookingExtraInfos[].isAttend").description("참석여부. true==참석, false==불참, null==미정"),
-                        fieldWithPath("bookingExtraInfos[].memo").description("추가 서비스 관련 메모. 200이내"),
-                        fieldWithPath("bookingExtraInfos[].attendDate").description("참석일. 필수"),
-                        fieldWithPath("bookingExtraInfos[].peopleCount").description("참석 인원수")
-                )
-        ));
+        resultActions.andExpect(status().isCreated())
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("houseId").description("요청하고자 하는 house id, 호스트가 속한 하우스가 아닐 경우 403 반환")
+                        ),
+                        requestFields(
+                                fieldWithPath("bookingId").description("예약 id"),
+                                fieldWithPath("checkIn").description("체크인 날짜. 필수"),
+                                fieldWithPath("checkOut").description("체크아웃 날짜. 필수"),
+                                fieldWithPath("requirement").description("요구사항. 500자 이내"),
+                                fieldWithPath("guest").description("게스트 Request 객체"),
+                                fieldWithPath("guest.guestId").description("게스트 id"),
+                                fieldWithPath("guest.name").description("게스트 이름. 필수"),
+                                fieldWithPath("guest.phoneNumber").description("게스트 전화번호. 필수"),
+                                fieldWithPath("guest.email").description("게스트 이메일"),
+                                fieldWithPath("guest.memo").description("게스트 메모. 500자 이내"),
+                                fieldWithPath("bookingRooms").description("예약한 방 배열"),
+                                fieldWithPath("bookingRooms[].roomId").description("선택한 방의 id. 필수"),
+                                fieldWithPath("bookingRooms[].femaleCount").description("여성 인원수"),
+                                fieldWithPath("bookingRooms[].maleCount").description("남성 인원수"),
+                                fieldWithPath("bookingExtraInfos").description("작성된 하우스 추가 서비스 배열"),
+                                fieldWithPath("bookingExtraInfos[].houseExtraInfoId").description("선택한 추가 서비스 id. 필수"),
+                                fieldWithPath("bookingExtraInfos[].bookingExtraInfoId").description("예약 추가 서비스 id"),
+                                fieldWithPath("bookingExtraInfos[].isAttend").description("참석여부. true==참석, false==불참, null==미정"),
+                                fieldWithPath("bookingExtraInfos[].memo").description("추가 서비스 관련 메모. 200이내"),
+                                fieldWithPath("bookingExtraInfos[].attendDate").description("참석일. 필수"),
+                                fieldWithPath("bookingExtraInfos[].peopleCount").description("참석 인원수")
+                        )
+                ));
     }
+
+    @Test
+    @DisplayName("게스트를 찾을 수 없음")
+    void cannotFoundGuest() throws Exception {
+        //given
+        BookingRequestDto request = BookingRequestDto.builder().checkOut(LocalDate.now().atStartOfDay()).checkIn(LocalDate.now().atStartOfDay()).build();
+        doThrow(new NotFoundDataException(ErrorCode.NOT_FOUND_GUEST)).when(bookingService).addBookingInfo(any(), any());
+
+        //when
+        ResultActions resultActions = create(request);
+
+        //then
+        resultActions.andExpect(status().isNotFound()).andExpect(jsonPath("code").value(ErrorCode.NOT_FOUND_GUEST.getCode()))
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
+
+    @Test
+    @DisplayName("방을 찾을 수 없음")
+    void cannotFoundRoom() throws Exception {
+        //given
+        BookingRequestDto request = BookingRequestDto.builder().checkOut(LocalDate.now().atStartOfDay()).checkIn(LocalDate.now().atStartOfDay()).build();
+        doThrow(new NotFoundDataException(ErrorCode.ROOM_NOT_FOUND)).when(bookingService).addBookingInfo(any(), any());
+
+        //when
+        ResultActions resultActions = create(request);
+
+        //then
+        resultActions.andExpect(status().isNotFound()).andExpect(jsonPath("code").value(ErrorCode.ROOM_NOT_FOUND.getCode()))
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
+    @Test
+    @DisplayName("인원이 0인 경우")
+    void zeroCount() throws Exception {
+        //given
+        BookingRequestDto request = BookingRequestDto.builder().checkOut(LocalDate.now().atStartOfDay()).checkIn(LocalDate.now().atStartOfDay()).build();
+        doThrow(new ZeroCountException(ErrorCode.ZERO_COUNT)).when(bookingService).addBookingInfo(any(), any());
+
+        //when
+        ResultActions resultActions = create(request);
+
+        //then
+        resultActions.andExpect(status().isBadRequest()).andExpect(jsonPath("code").value(ErrorCode.ZERO_COUNT.getCode()))
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
+    @Test
+    @DisplayName("재고를 찾을 수 없음")
+    void cannotFoundUnbooked() throws Exception {
+        //given
+        BookingRequestDto request = BookingRequestDto.builder().checkOut(LocalDate.now().atStartOfDay()).checkIn(LocalDate.now().atStartOfDay()).build();
+        doThrow(new NotFoundDataException(ErrorCode.NOT_FOUND_UNBOOKED)).when(bookingService).addBookingInfo(any(), any());
+
+        //when
+        ResultActions resultActions = create(request);
+
+        //then
+        resultActions.andExpect(status().isNotFound()).andExpect(jsonPath("code").value(ErrorCode.NOT_FOUND_UNBOOKED.getCode()))
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
+    @Test
+    @DisplayName("하우스 추가 정보를 찾을 수 없음")
+    void cannotFoundHouseExtra() throws Exception {
+        //given
+        BookingRequestDto request = BookingRequestDto.builder().checkOut(LocalDate.now().atStartOfDay()).checkIn(LocalDate.now().atStartOfDay()).build();
+        doThrow(new NotFoundDataException(ErrorCode.NOT_FOUND_HOUSE_EXTRA)).when(bookingService).addBookingInfo(any(), any());
+
+        //when
+        ResultActions resultActions = create(request);
+
+        //then
+        resultActions.andExpect(status().isNotFound()).andExpect(jsonPath("code").value(ErrorCode.NOT_FOUND_HOUSE_EXTRA.getCode()))
+                .andDo(document("{class-name}/{method-name}",
+                        preprocessRequest(modifyUris()
+                                .scheme(CommonString.SCHEMA)
+                                .host(CommonString.HOST), prettyPrint()),
+                        preprocessResponse(prettyPrint())
+                ));
+    }
+
 
     private ResultActions create(BookingRequestDto request) throws Exception {
         return mockMvc.perform(RestDocumentationRequestBuilders.post("/api/v1/houses/{houseId}/bookings", 1)
