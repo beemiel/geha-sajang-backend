f29368180882a6a589ac45c08bd21022cd89cda8
beemiel <qnswwrn@gmail.com>
2020-09-09 01:56:05 +0900

    refactor: ControllerAdvice Annotation 수정
    - ControllerAdvice에서 RestControllerAdvice로 수정

--- src/main/java/com/incense/gehasajang/error/ControllerErrorAdvice.java
+++ src/main/java/com/incense/gehasajang/error/ControllerErrorAdvice.java
@@ -10,26 +10,21 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerErrorAdvice {
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundDataException.class)
     public ErrorResponse handleNotFoundData(NotFoundDataException e) {
         return ErrorResponse.buildError(e.getErrorCode());
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
@@ -37,7 +32,6 @@ public class ControllerErrorAdvice {
         return buildValidationError(ErrorCode.INPUT_VALUE_INVALID, fieldErrors);
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ErrorResponse handleBindException(BindException e) {
@@ -45,35 +39,30 @@ public class ControllerErrorAdvice {
         return buildValidationError(ErrorCode.INPUT_VALUE_INVALID, fieldErrors);
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ErrorResponse handleFileSizeLimitException() {
         return ErrorResponse.buildError(ErrorCode.FILE_SIZE_LIMIT_EXCEED);
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(CannotConvertException.class)
     public ErrorResponse handleCannotConvertException() {
         return ErrorResponse.buildError(ErrorCode.CANNOT_CONVERT_FILE);
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ConstraintViolationException.class)
     public ErrorResponse handleDuplicateException() {
         return ErrorResponse.buildError(ErrorCode.DUPLICATE);
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
     public ErrorResponse handleAccessDenied(AccessDeniedException e) {
         return ErrorResponse.buildError(e.getErrorCode());
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(FailToAuthenticationException.class)
     public ErrorResponse handleFailToAuth(FailToAuthenticationException e) {
