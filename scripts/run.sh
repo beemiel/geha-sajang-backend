434f5973306fd781af8cf560a9ef90b8818936cd
beemiel <qnswwrn@gmail.com>
2021-01-14 00:31:23 +0900

    chore: application.yml delopy profile 추가
    - application.yml에서 profile을  local과 deploy로 분리함
    - build에는 local을 사용하고 배포시에는 deploy를 사용하도록 수정
    - 불필요한 부분 삭제
    - github의 secrets에서 aws rds 환경 변수를 가져오도록 추가

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# application.yml을 S3에서 가져옴(IAM으로 S3 FullAccess를 줬으므로 가능)
-echo "> Get Application.yml From S3"
-aws s3 cp s3://lynn-test-deploy/application.yml /home/ubuntu/geha-sajang/src/main/resources/
-
 # 기존거는 죽여버려
 # 중간에는 프로젝트 제목이니까 잘 바꿔주자
 echo "> Kill Current Application"
@@ -11,6 +7,6 @@ CURRENT_PID=$(ps -ef | grep geha-sajang | grep java | awk '{print $2}')
 sudo kill -15 ${CURRENT_PID}
 
 # 새로 어플리케이션 실행
+nohup java -jar -Dspring.profiles.active=deploy /home/ubuntu/geha-sajang/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs."
-nohup java -jar /home/ubuntu/geha-sajang/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 
