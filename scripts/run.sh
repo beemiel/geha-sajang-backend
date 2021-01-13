e2fb4029e35424b426dcfb4759edab107658d0f3
beemiel <qnswwrn@gmail.com>
2021-01-14 00:57:38 +0900

    chore: 배포 전 기존의 nohup.out 삭제

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,10 @@ echo "> Kill Current Application"
 CURRENT_PID=$(ps -ef | grep geha-sajang | grep java | awk '{print $2}')
 sudo kill -15 ${CURRENT_PID}
 
+# 기존의 nohup.out 삭제
+sudo rm /home/ubuntu/nohup.out
+echo "> Remove nohup.out"
+
 # 새로 어플리케이션 실행
 nohup java -jar -Dspring.profiles.active=deploy /home/ubuntu/geha-sajang/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs."
