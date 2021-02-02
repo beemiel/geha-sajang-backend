c446e43f996043f384ff9ca07e431a057bde17ad
beemiel <qnswwrn@gmail.com>
2021-02-02 20:10:01 +0900

    test: POST Rooms test 작성
    - 내부 디비에서 테스트 작성
    - @BeforeEach로 데이터 삽입
    - 각 메서드에서 테스트
    - find시 id를 사용하는 checkRoom이 먼저 실행되도록 설정

--- src/test/java/com/incense/gehasajang/domain/room/RoomRepositoryTest.java
+++ src/test/java/com/incense/gehasajang/domain/room/RoomRepositoryTest.java
@@ -1,22 +1,39 @@
 package com.incense.gehasajang.domain.room;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.incense.gehasajang.domain.house.House;
+import com.incense.gehasajang.domain.house.HouseRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RoomRepositoryTest {
 
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private HouseRepository houseRepository;
+
+    @BeforeEach
+    void setUp() {
+        House house = House.builder().name("새로 추가한 하우스").build();
+        houseRepository.save(house);
+
+        List<Room> rooms = Arrays.asList(Room.builder().house(house).name("새로 추가한 방1").roomType(RoomType.SINGLE).peakAmount("1000").offPeakAmount("800").maxCapacity(2).defaultCapacity(2).memo("방 메모1").build(),
+                Room.builder().house(house).name("새로 추가한 방2").roomType(RoomType.DORMITORY).peakAmount("2000").offPeakAmount("6700").maxCapacity(3).defaultCapacity(5).memo("방 메모2").build());
+        roomRepository.saveAll(rooms);
+    }
+
     @Test
     @DisplayName("방 유효성 검사")
+    @Order(1)
     void checkRoom() throws Exception {
         //when
         boolean result = roomRepository.existsByIdAndDeletedAtNullAndHouse_Id(1L, 1L);
@@ -25,6 +42,18 @@ class RoomRepositoryTest {
         //then
         assertThat(result).isTrue();
         assertThat(result2).isFalse();
+    }
+
+    @Test
+    @DisplayName("방 여러개 추가 검사")
+    @Order(2)
+    void saveRooms() throws Exception {
+        //when
+        List<Room> findRooms = roomRepository.findAll();
+
+        //then
+        assertThat(findRooms.size()).isEqualTo(2);
+        assertThat(findRooms.get(1).getName()).isEqualTo("새로 추가한 방2");
     }
 
 }
