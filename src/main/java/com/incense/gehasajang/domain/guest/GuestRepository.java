29816a3951b3f17371d22078bc918f1fb03d2ec9
beemiel <qnswwrn@gmail.com>
2021-02-03 23:02:28 +0900

    test: GuestRepositoryTest 테스트 수정
    - data.sql 삭제 후 데이터를 직접 넣어서 사용하도록 테스트를 수정
    - GuestRepository에 필요한 주석 추가

--- src/main/java/com/incense/gehasajang/domain/guest/GuestRepository.java
+++ src/main/java/com/incense/gehasajang/domain/guest/GuestRepository.java
@@ -15,6 +15,7 @@ public interface GuestRepository extends JpaRepository<Guest, Long> {
             value = "SELECT b.check_in FROM guest g JOIN booking b ON g.guest_id = b.guest_id WHERE g.guest_id = :guestId AND b.house_id = :houseId ORDER BY b.check_in DESC LIMIT 1")
     LocalDateTime findLastBookingById(@Param(value = "guestId") Long guestId, @Param(value = "houseId") Long houseId);
 
+    // TODO: 2021-02-03 list로 변경할 것
     Set<Guest> findAllByNameAndBookings_House(@Param(value = "name") String name, @Param(value = "house")House house);
 
     Optional<Guest> findByIdAndBookings_House(@Param(value = "guestId") Long guestId, @Param(value = "house") House house);
--- src/test/java/com/incense/gehasajang/domain/guest/GuestRepositoryTest.java
+++ src/test/java/com/incense/gehasajang/domain/guest/GuestRepositoryTest.java
@@ -1,22 +1,23 @@
 package com.incense.gehasajang.domain.guest;
 
+import com.incense.gehasajang.domain.booking.Booking;
+import com.incense.gehasajang.domain.booking.BookingRepository;
+import com.incense.gehasajang.domain.booking.Stay;
 import com.incense.gehasajang.domain.house.House;
 import com.incense.gehasajang.domain.house.HouseRepository;
 import com.incense.gehasajang.error.ErrorCode;
-import com.incense.gehasajang.exception.AccessDeniedException;
 import com.incense.gehasajang.exception.NotFoundDataException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
 class GuestRepositoryTest {
 
     @Autowired
@@ -25,14 +26,32 @@ class GuestRepositoryTest {
     @Autowired
     private GuestRepository guestRepository;
 
+    @Autowired
+    private BookingRepository bookingRepository;
+
     @Test
     @DisplayName("게스트 리스트 테스트")
     void list() throws Exception {
         //given
-        House house = houseRepository.findById(1L).orElseThrow(() -> new AccessDeniedException(ErrorCode.ACCESS_DENIED));
+        House house = House.builder().name("게스트 하우스").build();
+        houseRepository.save(house);
+
+        Guest guest = Guest.builder().name("test").phoneNumber("01011111111").build();
+        Guest guest2 = Guest.builder().name("test2").phoneNumber("01022222222").build();
+        Guest guest3 = Guest.builder().name("test").phoneNumber("01011111111").build();
+        guestRepository.save(guest);
+        guestRepository.save(guest2);
+        guestRepository.save(guest3);
+
+        Booking booking = Booking.builder().house(house).guest(guest).requirement("게스트 부킹1").stay(new Stay(LocalDateTime.now(), LocalDateTime.now())).build();
+        Booking booking2 = Booking.builder().house(house).guest(guest2).requirement("게스트 부킹2").stay(new Stay(LocalDateTime.now(), LocalDateTime.now())).build();
+        Booking booking3 = Booking.builder().house(house).guest(guest3).requirement("게스트 부킹3").stay(new Stay(LocalDateTime.now(), LocalDateTime.now())).build();
+        bookingRepository.save(booking);
+        bookingRepository.save(booking2);
+        bookingRepository.save(booking3);
 
         //when
-        Set<Guest> savedGuests = guestRepository.findAllByNameAndBookings_House("test", house);
+        Set<Guest> savedGuests = guestRepository.findAllByNameAndBookings_House(guest.getName(), house);
 
         //then
         assertThat(savedGuests.size()).isEqualTo(2);
@@ -42,13 +61,20 @@ class GuestRepositoryTest {
     @DisplayName("게스트 찾기")
     void findGuest() throws Exception {
         //given
-        House house = houseRepository.findById(1L).orElseThrow(() -> new AccessDeniedException(ErrorCode.ACCESS_DENIED));
+        House house = House.builder().name("게스트 하우스").build();
+        houseRepository.save(house);
+
+        Guest guest = Guest.builder().name("test").phoneNumber("01011111111").build();
+        guestRepository.save(guest);
+
+        Booking booking = Booking.builder().house(house).guest(guest).requirement("게스트 부킹1").stay(new Stay(LocalDateTime.now(), LocalDateTime.now())).build();
+        bookingRepository.save(booking);
 
         //when
-        Guest guest = guestRepository.findByIdAndBookings_House(2L, house).orElseThrow(() -> new NotFoundDataException(ErrorCode.NOT_FOUND_GUEST));
+        Guest findGuest = guestRepository.findByIdAndBookings_House(guest.getId(), house).orElseThrow(() -> new NotFoundDataException(ErrorCode.NOT_FOUND_GUEST));
 
         //then
-        assertThat(guest.getPhoneNumber()).isEqualTo("01011111111");
+        assertThat(findGuest.getPhoneNumber()).isEqualTo(guest.getPhoneNumber());
     }
 
 }
