a09656b92179b2a8bb3fa5a8977ed4f277638e61
beemiel <qnswwrn@gmail.com>
2021-02-03 23:10:42 +0900

    test: HouseRepositoryTest 수정
    - data.sql 삭제 후 데이터를 직접 삽입 후 테스트 하도록 수정
    - 하드코딩 수정

--- src/test/java/com/incense/gehasajang/domain/house/HouseRepositoryTest.java
+++ src/test/java/com/incense/gehasajang/domain/house/HouseRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.incense.gehasajang.domain.house;
 
+import com.incense.gehasajang.domain.host.Host;
+import com.incense.gehasajang.domain.host.HostRepository;
+import com.incense.gehasajang.domain.host.MainHost;
 import com.incense.gehasajang.error.ErrorCode;
 import com.incense.gehasajang.exception.AccessDeniedException;
 import org.junit.jupiter.api.DisplayName;
@@ -18,19 +21,34 @@ class HouseRepositoryTest {
     @Autowired
     private HouseRepository houseRepository;
 
+    @Autowired
+    private HostRepository hostRepository;
+
+    @Autowired
+    private HostHouseRepository hostHouseRepository;
+
     @Test
     @DisplayName("하우스 검증 테스트")
     void authHouse() throws Exception {
         //when
-        House savedHouse = houseRepository.findByIdAndHostHouses_Host_Account(1L, "bluuminn@gmail.com")
+        Host host = MainHost.builder().account("test@gmail.com").nickname("test").password("test").build();
+        hostRepository.save(host);
+
+        House house = House.builder().name("authHouse 하우스").build();
+        houseRepository.save(house);
+
+        HostHouse hostHouse = HostHouse.builder().host(host).house(house).build();
+        hostHouseRepository.save(hostHouse);
+
+        House savedHouse = houseRepository.findByIdAndHostHouses_Host_Account(house.getId(), host.getAccount())
                 .orElseThrow(() -> new AccessDeniedException(ErrorCode.ACCESS_DENIED));
 
         AccessDeniedException e = assertThrows(AccessDeniedException.class,
-                () -> houseRepository.findByIdAndHostHouses_Host_Account(2L, "foo@gmail.com")
+                () -> houseRepository.findByIdAndHostHouses_Host_Account(house.getId(), "foo@gmail.com")
                         .orElseThrow(() -> new AccessDeniedException(ErrorCode.ACCESS_DENIED)));
 
         //then
-        assertThat(savedHouse.getName()).isEqualTo("게스트 하우스1");
+        assertThat(savedHouse.getName()).isEqualTo(house.getName());
         assertThat(e.getErrorCode().getCode()).isEqualTo(ErrorCode.ACCESS_DENIED.getCode());
     }
 
