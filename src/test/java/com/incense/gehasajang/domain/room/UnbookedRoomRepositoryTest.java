26f33745c14a917bde2525be1366527d2d4e1cb5
beemiel <qnswwrn@gmail.com>
2021-02-03 23:24:48 +0900

    test: UnbookedRoomRepositoryTest 수정
    - data.sql 삭제 후 데이터를 직접 삽입 후 테스트하도록 수정
    - UnbookedRoomRepository에 필요한 주석 추가

--- src/test/java/com/incense/gehasajang/domain/room/UnbookedRoomRepositoryTest.java
+++ src/test/java/com/incense/gehasajang/domain/room/UnbookedRoomRepositoryTest.java
@@ -3,21 +3,23 @@ package com.incense.gehasajang.domain.room;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
 class UnbookedRoomRepositoryTest {
 
     @Autowired
     private UnbookedRoomRepository unbookedRoomRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
 
     private String date1 = "2020-08-01";
     private String date2 = "2020-08-02";
@@ -27,17 +29,27 @@ class UnbookedRoomRepositoryTest {
     @Test
     @DisplayName("재고 가져오기")
     void getUnbookedRoomByJpa() throws Exception {
+        //given
+        List<Room> rooms = Arrays.asList(Room.builder().name("방1").roomType(RoomType.SINGLE).peakAmount("1000").offPeakAmount("800").maxCapacity(2).defaultCapacity(2).memo("방 메모1").build(),
+                Room.builder().name("방2").roomType(RoomType.DORMITORY).peakAmount("2000").offPeakAmount("6700").maxCapacity(3).defaultCapacity(5).memo("방 메모2").build());
+        roomRepository.saveAll(rooms);
+
+        List<UnbookedRoom> unbookedRooms = Arrays.asList(
+                UnbookedRoom.builder().room(rooms.get(0)).todayAmount("100").entryDate(LocalDate.parse(date1, formatter).atStartOfDay()).build(),
+                UnbookedRoom.builder().room(rooms.get(1)).todayAmount("100").entryDate(LocalDate.parse(date2, formatter).atStartOfDay()).build(),
+                UnbookedRoom.builder().room(rooms.get(1)).todayAmount("100").entryDate(LocalDate.parse(date2, formatter).atStartOfDay()).build()
+        );
+        for (UnbookedRoom unbookedRoom : unbookedRooms) {
+            unbookedRoomRepository.save(unbookedRoom);
+        }
+
         //when
-        List<UnbookedRoom> unbookedRooms1 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date1, formatter).atStartOfDay(), LocalDate.parse(date1, formatter).atStartOfDay(), 4L);
-        List<UnbookedRoom> unbookedRooms2 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date1, formatter).atStartOfDay(), LocalDate.parse(date2, formatter).atStartOfDay(), 4L);
-        List<UnbookedRoom> unbookedRooms3 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date1, formatter).atStartOfDay(), LocalDate.parse(date3, formatter).atStartOfDay(), 4L);
-        List<UnbookedRoom> unbookedRooms4 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date1, formatter).atStartOfDay(), LocalDate.parse(date3, formatter).atStartOfDay(), 5L);
+        List<UnbookedRoom> unbookedRooms1 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date1, formatter).atStartOfDay(), LocalDate.parse(date3, formatter).atStartOfDay(), rooms.get(0).getId());
+        List<UnbookedRoom> unbookedRooms2 = unbookedRoomRepository.findAllByEntryDateBetweenAndRoom_IdAndRoom_DeletedAtNullAndBookedRoom_UnbookedRoomNull(LocalDate.parse(date2, formatter).atStartOfDay(), LocalDate.parse(date3, formatter).atStartOfDay(), rooms.get(1).getId());
 
         //then
         assertThat(unbookedRooms1.size()).isEqualTo(1);
         assertThat(unbookedRooms2.size()).isEqualTo(2);
-        assertThat(unbookedRooms3.size()).isEqualTo(3);
-        assertThat(unbookedRooms4.size()).isEqualTo(0);
     }
 
 }
